3c05d717df285a7e5055bfd5af02709d76099413
Emilia Zawadka <ezawadka236@gmail.com>
2023-06-18 19:59:06 +0200

    6.1 Added kodilla-testing module

--- src/main/java/com/kodilla/testing/TestingMain.java
+++ src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,4 +1,5 @@
-package com.kodilla;
+package com.kodilla.testing;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
