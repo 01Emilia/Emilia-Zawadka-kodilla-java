79aa56848fe9c275fd27857a6aea3b1d942edce2
Emilia Zawadka <ezawadka236@gmail.com>
2023-07-16 14:55:04 +0200

    First commit

---
+++ kodilla-testing/src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla;
+
+public class Calculator {
+
+        int add(int a, int b){
+            return a + b;
+        }
+        int subtract(int a, int b){
+            return a - b;
+        }
+        int subtract1(int a, int b){
+        return b - a;
+        }
+
+
+}
+
--- kodilla-testing/src/main/java/com/kodilla/Main.java
+++ kodilla-testing/src/main/java/com/kodilla/Main.java
@@ -1,19 +1,17 @@
 package com.kodilla;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Calculator c = new Calculator();
+        int a = 5;
+        int b = 10;
+        int result = c.add(a, b);
+        int result1 = c.subtract(a, b);
+        int result2 = c.subtract1(a, b);
+        System.out.println( a + "+" + b + "=" + result);
+        System.out.println( a + "-" + b + "=" +result1);
+        System.out.println( b + "-" + a + "=" +result2);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
